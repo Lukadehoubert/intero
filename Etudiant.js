1f32de53b5f4da58bc02fe3d5b7358e9d82dc3f3
Luka Dehoubert <luka.dehoubert@cesjb.be>
2025-11-20 15:27:49 +0100

    coucou toi

---
+++ Etudiant.js
@@ -0,0 +1,14 @@
+class Etudiant {
+    // TODO : compléter la classe
+    nom 
+    age
+    section
+       constructor(nom,age,section){
+        this.nom = nom
+        this.age = age
+        this.section = section
+    };
+}
+
+MediaSourceHandle.exports = Etudian; // si projet Node
+// ou export default Etudiant; pour un projet front 
